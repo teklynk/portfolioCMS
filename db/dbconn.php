91ab01e45fb3d0a00e11e44a92c39e6bd9d95a91
Ryan Jones <ryanjones153@gmail.com>
2015-02-17 19:06:07 -0500

    Small fix

--- db/dbconn.php
+++ db/dbconn.php
@@ -5,6 +5,7 @@ $db_username = "";
 $db_password = "";
 $db_name = "";
 
+
 //Global Vars - Edit values for your web site. leave as is in most cases.
 $image_dir = "../uploads/"; //physical path to uploads folder
 $image_url = "http://".$_SERVER['HTTP_HOST']."/uploads/"; //web path to uploads folder
