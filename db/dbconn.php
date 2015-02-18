98606752b2209f42da3857b57769b7fbfacc3809
Ryan Jones <ryanjones153@gmail.com>
2015-02-17 19:10:15 -0500

    Small fix

--- db/dbconn.php
+++ db/dbconn.php
@@ -6,18 +6,15 @@ $db_password = "";
 $db_name = "";
 
 
-
 //Global Vars - Edit values for your web site. leave as is in most cases.
 $image_dir = "../uploads/"; //physical path to uploads folder
 $image_url = "http://".$_SERVER['HTTP_HOST']."/uploads/"; //web path to uploads folder
 $target_dir = $image_dir;
 
 
-
 //Custom CSS file paths
 $customCss_dir = '../css/custom.css'; //physical path to custom css file
 $customCss_url = "http://".$_SERVER['HTTP_HOST']."/css/custom.css"; //web path to custom css file
-
 
 
 //establish db connection 
