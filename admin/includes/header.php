5b4f3d8b49258005b82aaf7ed6304f48a369db5c
Ryan Jones <ryanjones153@gmail.com>
2015-03-01 02:27:37 -0500

    moved include dbconn.php further up the code, after the head tag.

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<?php
+session_start();
+//DB connection string and Global variable
+include '../db/dbconn.php'; 
+?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -29,10 +34,6 @@
     <![endif]-->
 
   <?php
-	session_start();
-	//DB connection string and Global variable
-	include '../db/dbconn.php'; 
-	
 	$sqlSetup = mysql_query("SELECT tinymce FROM setup");
 	$row  = mysql_fetch_array($sqlSetup);
 
