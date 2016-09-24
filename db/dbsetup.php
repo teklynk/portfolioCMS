730d6b8c9ba0ac32568b2d0934709e87a79dc668
teklynk <ryanjones153@gmail.com>
2016-09-23 21:10:07 -0400

    modified dbsetup

--- db/dbsetup.php
+++ db/dbsetup.php
@@ -3,12 +3,12 @@ include 'dbconn.php';
 
 //Global Vars - Edit values for your web site. leave as is in most cases.
 $image_dir = "../uploads/"; //physical path to uploads folder
-$image_url = "//".$_SERVER['HTTP_HOST']."/uploads/"; //web path to uploads folder
+$image_url = "//".$_SERVER['HTTP_HOST']."/portfolioCMS/uploads/"; //web path to uploads folder
 $target_dir = $image_dir;
 
 //Custom CSS file paths
 $customCss_dir = '../css/custom.css'; //physical path to custom css file
-$customCss_url = "//".$_SERVER['HTTP_HOST']."/css/custom.css"; //web path to custom css file
+$customCss_url = "//".$_SERVER['HTTP_HOST']."/portfolioCMS/css/custom.css"; //web path to custom css file
 
 //establish db connection
 $db_conn = mysql_connect($db_servername, $db_username, $db_password);
